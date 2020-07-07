c71ed2c6d1e478cd205ce9c6815f32fd95109065
mishanwong <32437766+mishanwong@users.noreply.github.com>
2020-07-07 13:43:13 -0700

    Initial Commit

---
+++ BBGTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BBGTimer
+//
+//  Created by Mishan Wong on 7/7/20.
+//  Copyright © 2020 Mishan Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
